9018ad42801b4ce27d64bc8d96e34e3ddc8ee9f6
David Iglesias <david.iglesias@gmail.com>
2020-10-11 19:36:29 +0200

    Reorganizando las plantillas de twig

--- src/Dependencies.php
+++ src/Dependencies.php
@@ -15,23 +15,23 @@ $injector->define('Http\HttpRequest', [
 $injector->alias('Http\Response', 'Http\HttpResponse');
 $injector->share('Http\HttpResponse');
 
+// $injector->alias('NoFrameWorkTutorial\Template\Renderer', 'NoFrameWorkTutorial\Template\MustacheRenderer');
+// $injector->define('Mustache_Engine', [
+//     ':options' => [
+//         'loader' => new Mustache_Loader_FilesystemLoader(dirname(__DIR__) . '/templates', [
+//             'extension' => '.html',
+//         ]),
+//     ],
+// ]);
+// $injector->alias('NoFrameWorkTutorial\Template\FrontendRenderer', 'NoFrameWorkTutorial\Template\FrontendMustacheRenderer');
+
 $injector->alias('NoFrameWorkTutorial\Template\Renderer', 'NoFrameWorkTutorial\Template\TwigRenderer');
-
-$injector->define('Mustache_Engine', [
-    ':options' => [
-        'loader' => new Mustache_Loader_FilesystemLoader(dirname(__DIR__) . '/templates', [
-            'extension' => '.html',
-        ]),
-    ],
-]);
-
 $injector->delegate('Twig_Environment', function () use ($injector) {
-    $loader = new Twig_Loader_Filesystem(dirname(__DIR__) . '/templates');
+    $loader = new Twig_Loader_Filesystem(dirname(__DIR__) . '/templates/twig');
     $twig = new Twig_Environment($loader);
     return $twig;
 });
 $injector->alias(\Twig\Environment::class, 'Twig_Environment');
-
 $injector->alias('NoFrameWorkTutorial\Template\FrontendRenderer', 'NoFrameWorkTutorial\Template\FrontendTwigRenderer');
 
 $injector->define('NoFrameWorkTutorial\Page\FilePageReader', [
